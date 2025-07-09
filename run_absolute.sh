db97f8349470f3d303ca3f704532b0e38d04da64
ProDevJune <gd2u37@gmail.com>
2025-07-09 17:15:10 +0900

    run_absolute.sh 수정: 프로젝트 디렉토리로 올바르게 이동하도록 수정

--- run_absolute.sh
+++ run_absolute.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# 절대 경로로 완전 해결
-
-cd 
+# 절대 경로로 완전 해결 - 수정됨
 
 echo "🎯 절대 경로로 EfficientNet-B4 실험 시작"
+
+# 프로젝트 디렉토리로 이동
+cd /data/ephemeral/home/upstageailab-cv-classification-cv_5
 
 # 절대 경로로 설정 파일 지정
 venv/bin/python codes/gemini_main.py --config codes/practice/exp_golden_efficientnet_b4_202507051902.yaml
